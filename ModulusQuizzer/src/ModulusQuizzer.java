c9ddf0e07f3ec4c2ac5298674eda2a70a04d30ae
Dylan.Insalaco <Dylan.Insalaco@MU-C1-20.mullen.pvt.k12.co.us>
2016-04-29 09:42:41 -0600

    initial commit

---
+++ ModulusQuizzer/src/ModulusQuizzer.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class ModulusQuizzer
+	{
+
+		public static void main(String[] args)
+			{
+			int counter=0;
+			System.out.println("how many questions do you want to ask");
+			Scanner userInput = new Scanner(System.in);
+			
+				
+			int x= userInput.nextInt();
+			for(int i=0;i<x;i++)
+				{
+				int rn1 = (int)(Math.random()*5) + 1;
+				int rn2 = (int)(Math.random()*5) + 1;
+				System.out.println(rn1 +" % " + rn2 + "=" +"?");
+				
+				
+			Scanner userInput1 = new Scanner(System.in);
+				int A=userInput1.nextInt();
+				if (rn1%rn2==A)
+					{
+					System.out.println("correct");
+				counter++;
+					}
+				else
+				    System.out.println("incorrect");
+				
+				}
+			System.out.println("you got " + counter + " correct ");
+			System.out.println("that is a " + counter%x + "%");
+			}
+
+	}
